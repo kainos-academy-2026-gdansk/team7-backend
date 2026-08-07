e328d0798a7cb63002be19b5514b3735d6ae9647
wiktorlemanski <wiktor.lemanski@kainos.com>
2026-08-07 12:39:59 +0200

    test endpoint and comment removed

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,5 @@
 import express from "express";
 import type { Request, Response } from "express";
-import Logger from "./lib/logger";
 import morganMiddleware from "./middlewares/morganMiddleware";
 const app = express();
 
@@ -9,13 +8,7 @@ app.use(express.json());
 app.use(morganMiddleware);
 
 app.get("/health", (_req: Request, res: Response) => {
-  // Logger.warn("Health check endpoint was called - custon warn");
   return res.json({ status: "UP", timestamp: new Date().toISOString() });
-});
-
-app.post("/login", (req: Request, res: Response) => {
-  Logger.info(`${req.body.login}`);
-  return res.json({ test: "test" });
 });
 
 export default app;
