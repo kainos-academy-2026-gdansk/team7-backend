94db27b9b04d5dcc6a7e1d6715a738cc9ab0e6fe
wiktorlemanski <wiktor.lemanski@kainos.com>
2026-08-07 12:35:09 +0200

    added Morgan and Winston for logs

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,21 @@
 import express from "express";
 import type { Request, Response } from "express";
+import Logger from "./lib/logger";
+import morganMiddleware from "./middlewares/morganMiddleware";
 const app = express();
 
 app.use(express.json());
 
+app.use(morganMiddleware);
+
 app.get("/health", (_req: Request, res: Response) => {
+  // Logger.warn("Health check endpoint was called - custon warn");
   return res.json({ status: "UP", timestamp: new Date().toISOString() });
+});
+
+app.post("/login", (req: Request, res: Response) => {
+  Logger.info(`${req.body.login}`);
+  return res.json({ test: "test" });
 });
 
 export default app;
---
+++ src/middlewares/morganMiddleware.ts
@@ -0,0 +1,19 @@
+import morgan, { type StreamOptions } from "morgan";
+import Logger from "../lib/logger";
+
+const stream: StreamOptions = {
+  write: (message) => Logger.http(message),
+};
+const skip = () => {
+  const env = process.env.NODE_ENV || "development";
+  return env !== "development";
+};
+
+const morganMiddleware = morgan(
+  ":method :url :status :res[content-length] chars - :response-time ms [morgan]",
+  {
+    stream,
+    skip,
+  },
+);
+export default morganMiddleware;
