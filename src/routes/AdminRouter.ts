4cd32533e7b0a25226ffc33f02f12b7ffd5a0d9a
wiktorlemanski <wiktor.lemanski@kainos.com>
2026-08-17 13:47:18 +0200

    job roles admin operation separated

--- src/routes/AdminRouter.ts
+++ src/routes/AdminRouter.ts
@@ -1,7 +1,9 @@
 import { Role } from "@prisma/client";
 import express from "express";
 import { UpdateApplicationStatusSchema } from "../Dto/ApplicationDTO";
+import { AddJobRoleSchema, updateJobRoleSchema } from "../Dto/JobRoleDTO";
 import { ApplicationController } from "../controllers/ApplicationController";
+import { JobRoleController } from "../controllers/JobRoleController";
 import { authenticate, authorize } from "../middlewares/AuthMiddleware";
 import {
   applicationParamsSchema,
@@ -11,11 +13,39 @@ import {
 } from "../middlewares/ValidationMiddleware";
 import prisma from "../prismaClient";
 import { ApplicationService } from "../services/ApplicationService";
+import { JobRoleService } from "../services/JobRoleService";
 
 const router = express.Router();
 
 const applicationService = new ApplicationService(prisma);
 const applicationController = new ApplicationController(applicationService);
+const jobRoleService = new JobRoleService(prisma);
+const jobRoleController = new JobRoleController(jobRoleService);
+
+router.post(
+  "/job-roles",
+  authenticate,
+  authorize(Role.ADMIN),
+  validateBody(AddJobRoleSchema),
+  jobRoleController.addJobRole,
+);
+
+router.put(
+  "/job-roles/:id",
+  authenticate,
+  authorize(Role.ADMIN),
+  validateParams(idParamSchema),
+  validateBody(updateJobRoleSchema),
+  jobRoleController.updateJobRole,
+);
+
+router.delete(
+  "/job-roles/:id",
+  authenticate,
+  authorize(Role.ADMIN),
+  validateParams(idParamSchema),
+  (req, res, next) => jobRoleController.deleteJobRole(req, res, next),
+);
 
 router.get(
   "/applications",
